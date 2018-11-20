f5f9cd5252a492548edf28747b4650434339404d
Martín <martin.lozano.gomez@cieep.com>
2018-11-20 11:28:05 +0100

    Initial commit

---
+++ app/src/main/java/es/martinlozano/ejemplo10_navigationyfragments/fragments/ManageFragment.java
@@ -0,0 +1,57 @@
+package es.martinlozano.ejemplo10_navigationyfragments.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.ref.PhantomReference;
+
+import es.martinlozano.ejemplo10_navigationyfragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ManageFragment extends Fragment {
+
+    private ImageView ivImagen;
+    private Button btnManage;
+    private TextView txtManage;
+    private Context contexto;
+
+    public ManageFragment() {
+        // Required empty public constructor
+        contexto = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_manage, container, false);
+
+        // CÓDIGO NECESARIO PARA QUE LE FRAME FUNCIONE
+
+        txtManage = rootView.findViewById(R.id.txtManage);
+        btnManage = rootView.findViewById(R.id.btnManage);
+        ivImagen = rootView.findViewById(R.id.ivImagen);
+
+        btnManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(contexto, "HOLA MUNDO", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+    }
+
+}
